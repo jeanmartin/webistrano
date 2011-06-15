a77a819fb09d93656977c64edbfa774713ceffae
Jan Schwenzien <jan@schwenzien.info>
2011-06-15 10:51:13 +0200

    added secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webistrano::Application.config.secret_token = 'e0682bafb909a3467ec4acb7f00cbecf637d378c79c2963c8fc4bbd415eb9ec50acb0c3b32cd73db541078803dbdeddedf744ffb9ae357d0e31812f1424fb1bb'
