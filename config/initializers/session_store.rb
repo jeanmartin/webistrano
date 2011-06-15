648010ebb31ccb72493f5b404baa7ea968939cf0
Jan Schwenzien <jan@schwenzien.info>
2011-06-15 10:53:20 +0200

    changed cookie session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Webistrano::Application.config.session_store :cookie_store, :key => '_webistrano_session'
+Webistrano::Application.config.session_store :cookie_store, :key => '_webistrano'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
